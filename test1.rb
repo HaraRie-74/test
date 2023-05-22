4dd68b45a016c354c06600239235bd38b8286b51
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-05-22 14:27:32 +0000

    [add]ハノイの塔

--- test1.rb
+++ test1.rb
@@ -33,15 +33,10 @@
 # end
 
 
-#   array = Array.new(1..10)
-#   # puts array.inject(1) {|f, i| f * i}
-
-# p array
-
-array = (0..10).to_a
-array.each do |n|
-  puts "#{n}: #{(1..n).inject(1){|f, i| f * i}}"
-end
+# array = (0..10).to_a
+# array.each do |n|
+#   puts "#{n}: #{(1..n).inject(1){|f, i| f * i}}"
+# end
 
 
 # array = 1..6
@@ -64,3 +59,75 @@ end
 
 # array = (1..10).map{|i| i*2 + 1}
 # p array
+
+
+
+
+#50までのトリボナッチ数を求める
+# a = Array.new(51)
+# a[0] = 0
+# a[1] = 0
+# a[2] = 1
+# n = 3
+# while n <= 51 do
+#   a[n] = a[n-1] + a[n-2] + a[n-3]
+#   n += 1
+# end
+# puts a
+
+
+
+# # 2のN乗を計算する
+# for n in (0..10) do
+#   puts 2 ** n
+# end
+
+
+
+
+# Nの階乗を計算する
+# for n in (0..10) do
+#   a = 1
+#   for i in (1..n) do
+#     a = a * i
+#   end
+#   puts a
+# end
+
+# array = (0..20).to_a
+# array_2 = []
+# array.each do |n|
+#   array_2[n] = array[1..n].inject(1){|result, item| result * item}
+# end
+# puts array_2[10]
+
+
+
+
+#50までのトリボナッチ数を求める
+# a = (0..50).to_a
+# a[0] = 0
+# a[1] = 0
+# a[2] = 1
+
+# (3..50).map do |n|
+#   a[n] = a[n-1] + a[n-2] + a[n-3]
+# end
+
+# puts a[11]
+
+
+
+
+# ハノイの塔
+def hanoi(n, from, to, via)
+  if n == 1
+    puts "#{from} から #{to} へ移す"
+  else
+    hanoi(n - 1, from, via, to)
+    puts "#{from} から #{to} へ移す"
+    hanoi(n - 1, via, to, from)
+  end
+end
+
+hanoi(2, :a, :b, :c)
