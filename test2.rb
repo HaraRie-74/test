b50a456e538240eaeefcc33a89438eec038226e7
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-05-22 16:51:56 +0000

    [test]

---
+++ test2.rb
@@ -0,0 +1,44 @@
+# string = []
+# array = (1..100000).to_a
+# array.map do |n|
+#   string[n] = "#{n}SHEEP"
+# end
+
+# puts string.join.slice(99998, 20)
+
+
+
+# int = 387420489
+# for i in (1..40) do
+#   int = (int * 0.8).to_i
+# end
+# puts int
+
+
+
+
+# int = 0
+# (1..3333).each do |n|
+#   if (n % 3 == 0) || (n.to_s.length == 3) || (n.to_s.include?("3"))
+#     int += n
+#   end
+# end
+
+# puts int
+
+
+
+
+# array = (1..800).to_a.reverse
+
+# kg = 0
+#   while kg <= 5000 do
+#     kg = array.inject(0) {|n, kg| n + kg}
+#     puts kg
+#   end
+
+# for i in (800..1)
+#   while i * n <= 5000 do
+
+#   end
+# end
