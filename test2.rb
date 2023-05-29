e211da871d29d7cc4363bdf6ff60ace4099fd4d9
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-05-29 03:20:45 +0000

    paiza問題

--- test2.rb
+++ test2.rb
@@ -29,18 +29,45 @@
 
 
 
-array = (1..800).to_a.reverse
+# array = (1..800).to_a.reverse
 
-s = 0
+# s = 0
 
-kg = 0
-  while kg <= 5000 do
-    kg = array.inject(0) {|n, kg| n + kg}
-    puts kg
-  end
+# kg = 0
+#   while kg <= 5000 do
+#     kg = array.inject(0) {|n, kg| n + kg}
+#     puts kg
+#   end
 
 # for i in (800..1)
 #   while i * n <= 5000 do
 
 #   end
+# end
+
+
+
+
+
+# あなたは友人に向けてメールを送ろうとしています。
+# ただメールを送るだけではつまらないので、文字列を装飾して送ることにしました。
+# 送りたい文字列の周囲を "+" で枠のように囲んで装飾します。
+
+# このような処理を手作業で行いたくないため、プログラムで装飾しようとしています。
+# この "+" の枠で囲む装飾をするプログラムを書いてください。
+
+# input = gets.chomp
+# mozi = input.length.to_i
+# array = Array.new(3).map{Array.new(mozi + 2, "+")}
+# aa = input.chars
+
+# array[1] = aa
+# array[1].unshift("+")
+# array[1].push("+")
+
+# array.map do |b|
+#     b.each do |c|
+#         print c
+#     end
+#     puts ""
 # end
